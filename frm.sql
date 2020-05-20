ffc4dcacc58cd46ccf93a82b1f790eecc3cc00c8
Randy Choc <rchocm@miumg.edu.gt>
2020-05-19 19:36:29 -0600

    Prueba Bancos - Randy

--- frm.sql
+++ frm.sql
@@ -1,32 +1,7 @@
--- phpMyAdmin SQL Dump
--- version 4.7.4
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 20-05-2020 a las 02:23:48
--- Versión del servidor: 10.1.26-MariaDB
--- Versión de PHP: 7.1.9
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `frm`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `ayuda`
---
+  
+CREATE DATABASE IF NOT EXISTS frm;
+USE frm;
 
 CREATE TABLE `ayuda` (
   `Id_ayuda` int(11) NOT NULL,
@@ -1288,6 +1263,3 @@ ALTER TABLE `presupuesto_detalles`
   ADD CONSTRAINT `fk_tbl_presupuesto_detalle_tbl_presupuesto_Encabezado1` FOREIGN KEY (`id_presupuesto`) REFERENCES `presupuesto_encabezados` (`id_presupuesto`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
